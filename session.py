db849ea5e5ada4610de7f04e1af0e05257afac18
ZhangYiDa <thisforeda@live.cn>
2017-01-31 21:01:27 +0800

    Update session.py

--- session.py
+++ session.py
@@ -8,7 +8,7 @@ class SessionStore (object):
         if not hasattr (SessionStore, 'db'):
             
             SessionStore.db = redis.StrictRedis(
-                **settings.get (
+                ** settings.get (
                     'redis_session',
                     {
                         'host' : '127.0.0.1',
@@ -17,46 +17,40 @@ class SessionStore (object):
                         'db' : 0,
                     })
                 )
-'''
-    usage :
-    
-    app = tornado.web.Application(
-        handlers = [ 
-            (r'/t', TT),
-            (r'/e', EE),
-            (r'/c', CC)
-        ],
-        redis_session = dict(
-            host = 'redis server host', 
-            port = 6379, # port
-            db = 0, # db index
-            password = "passwd , (None) no passwd",
-            ),
-        debug = True,
-        )
-'''
 
+'''
+    产生一个 session id, 结果是一个随机数 + "_" + uuid4 结果
+    想遇到一个相同的结果可能性(对我来说忽略掉了)
+'''
 gen_sessionid = lambda : "_".join([
             "0123456789"[rand(0, 9)],  uuid.uuid4().hex
         ]) 
  
 class Session (dict):
-    
+    '''
+        Seesion, 属性和字典基本相同
+        SESSION_KEY 是存储在客户端中 session cookie 的名称
+    '''
+
     SESSION_KEY = 'session'
     
     def __init__(self, req):
+
         dict.__init__ (self)
         
-        # 如果是新会话的话需要客户端设置COOKIE, 否则没有必要
+        # 暂存 RequestHandler 用来后续 save()/clear() 使用
+        self._req_ = req
+
+        # 如果是新会话的话需要客户端设置COOKIE, 否则不需要
         self._newcookie_ = False
         
-        # redis 数据库
+        # 获取数据库连接
         self.redis = SessionStore(req.settings).db
-        
+
+        # 从数据库中提取 session 信息, 会话将会被 update 到自身
         self.load_session_from_db (req)
 
-        # RequestHandler
-        self.req = req
+
         
     def __getitem__ (self, k):
         '''
@@ -65,7 +59,7 @@ class Session (dict):
            如果不存在这个 key 那么会返回 None
         '''
         
-        return self.get(k, None)
+        return self.get (k, None)
     
     def load_session_from_db(self, req):
         '''
@@ -93,10 +87,14 @@ class Session (dict):
                     return
 
                 except :
+
+                    # 如果这中间遇到异常的话会重新生成 session
                     pass
 
-        # 否则新建一个会话 id
+        # 新建一个会话 id
         self.session_id = gen_sessionid()
+
+        # 标记这是一个新会话, 通知 save() 对客户端设置 cookie
         self._newcookie_ = True
         
         return
@@ -110,7 +108,7 @@ class Session (dict):
             
             #  设置 cookie, 以及 cookie 过期时间,
             #  一开始 expires 以为是过期秒数, 然后发现不论设置多大, 浏览器
-            #  cookie 失效日期都是 1970 年, 后来发现原来是 当前 time() + 要保存的秒数
+            #  cookie 失效日期都是 1970 年, 后来发现是当前 time() + 要保存的秒数
             if self._newcookie_ or expires:
 
                 if expires == None :
@@ -122,12 +120,12 @@ class Session (dict):
                 #  设置 session_id 在 redis 中的过期时间
                 self.redis.expire (self.session_id, expires)
                 
-                self.req.set_cookie (
+                self._req_.set_cookie (
                     Session.SESSION_KEY,
                     self.session_id,
                     expires = _time() + expires
                     )
-
+    
     def clear (self):
         '''
         class IndexHandler(tornado.web.RequestHandler, SessionMixin):
@@ -139,13 +137,14 @@ class Session (dict):
         
         '''
         
-        delattr (self.req, '_session_')
+        delattr (self._req_, '_session_')
         
         if self.redis.delete (self.session_id):
             
-            self.req.clear_cookie (
+            self._req_.clear_cookie (
                 Session.SESSION_KEY
                 )
+
 
 class SessionMixin (object):
     '''
@@ -168,10 +167,14 @@ class SessionMixin (object):
     '''
     
     @property
-    def session(self):
+    def session (self):
+        '''
+            没有必要每次访问 session 时都重新加载一下,
+            所以当存在_session_属性时直接返回会话。
+        '''
 
-        if not hasattr(self, '_session_'):
-            self._session_ = Session(self)
+        if not hasattr (self, '_session_'):
+            self._session_ = Session (self)
 
         return self._session_
     
